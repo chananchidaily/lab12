9949eb3512448ef174aafebf1d57cf482f4d1018
tonkhao <chananchida_tha@cmu.ac.th>
2023-01-06 15:39:37 +0700

    kkkk

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,27 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std ;
+
+int main (){
+    srand(time(0));
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    int A = rand()%9 ;
+
+    string G ;
+    if (A == 0) G = "A" ;
+    else if (A == 1) G = "B+" ;
+    else if (A == 2) G = "B";
+    else if (A == 3) G = "C+" ;
+    else if (A == 4) G = "C" ;
+    else if (A == 5) G = "D+" ;
+    else if (A == 6) G = "D" ;
+    else if (A == 7) G = "F" ;
+    else if (A == 8) G = "W" ;
+
+     cout << "You will get "<< G << " in this 261102.";
+}
